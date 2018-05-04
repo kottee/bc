c560ec3a47413eeef445b33c6b155d6443429ac6
kottee <kotteeswaranviit@gmail.com>
2018-05-04 13:02:29 +0530

    Update ContentController.php

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -144,7 +144,7 @@ class ContentController extends Controller
 			//	$items_create[] = $item;
 			//}
 			$tt = $it->show($item->itemBase->id);
-			$itemstockData = $itemstock->listStockByWarehouse($item->variationBase->id);
+			$itemstockData = $itemstock->listStockByWarehouse($item->variationBase->id, ['variationId','warehouseId','valueOfGoods','purchasePrice','physicalStock','reservedStock','netStock']);
 		    $items[] = $item;
 			$product[] = array(
 			  'name'=>$item->itemDescription->name1,
