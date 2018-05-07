e353856f9389a26879f8694c2ab46160c24c04f8
kottee <kotteeswaranviit@gmail.com>
2018-05-07 15:01:57 +0530

    Add files via upload

---
+++ src/Contracts/ItemDataProviderContract.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bc\Contracts;
+
+use Plenty\Modules\Item\DataLayer\Models\RecordList;
+
+/**
+ * Interface ItemDataProvider
+ */
+interface ItemDataProviderContract
+{
+	/**
+	 * @param $params
+	 *
+	 * @return RecordList
+	 */
+	public function fetch(array $params = []):RecordList;
+}
