eaf3116344f91decb8bbcfb9b70d9dd6749bb183
kottee <kotteeswaranviit@gmail.com>
2018-05-10 10:53:34 +0530

    Update ContentController.php

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -227,6 +227,12 @@ class ContentController extends Controller
 				'orderParamsMarkup',
 				'orderParamsMarkupNet'
 			],
+			'VariationStock' => [
+				'warehouseId',
+				'stockPhysical',
+				'stockNet',
+				'warehouseType'
+			],			
 			'variationMarketStatus' => [
 				'id',
 				'sku',
